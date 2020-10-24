0a3246052e8662e50fea76917330ec3e60e4df51
ivandangelo <ivanuntref@gmail.com>
2020-10-24 01:15:11 -0300

    inicio v1.0

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -19,15 +19,26 @@ var app = new Framework7({
         path: '/index/',
         url: 'index.html',
       },
+      {
+        path: '/anotador/',
+        url: 'anotador.html',
+
+
+      },
     ]
-    // ... other parameters
+
   });
 
 var mainView = app.views.create('.view-main');
+var router = mainView.router;
+//console.log(router);
+
+var j1 = '';
+var j2 = '';
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+    //console.log("Device is ready!");
 });
 
 // Option 1. Using one 'page:init' handler for all pages
@@ -36,9 +47,33 @@ $$(document).on('deviceready', function() {
     console.log(e);
 })*/
 
+
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="inicio"]', function (e) {
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    //console.log(e);
+    console.log("index ready");
+    $$('#ini').on('click',function(){
+        n = $$('#njds').val();
+        console.log(n);
+        j1 = $$('#j1in').val();
+        j2 = $$('#j2in').val();
+        if(j1=='' || j2==''){
+            app.dialog.alert('Completa todos los campos', 'Anotador');
+            /*app.dialog.alert('Completa todos los campos', 'Anotador',function(){
+                console.log('cuando apreto ok?')}); <-- se ejecuta cuando apreto el 'ok'
+                del dialog*/
+        }else {
+            router.navigate('/anotador/');
+
+        }
+
+    });
+
 })
+
+$$(document).on('page:init', '.page[data-name="anotador"]',function(e){
+    //console.log(e);
+    console.log("anotador ready");
+
+});
